e11601a2698e391369f032d382e3ef65b60285bd
Malcolm Duck <m.o.duck.15@aberdeen.ac.uk>
2020-07-05 13:31:26 +0000

    linked awsc9 development server address

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.hosts << "d038e690e425479db71ec90d6f1b218a.vfs.cloud9.us-east-2.amazonaws.com"
 end
