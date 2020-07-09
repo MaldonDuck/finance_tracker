3307948bd0e8c0c133b8d33f276f7ede4d20a801
Malcolm Duck <m.o.duck.15@aberdeen.ac.uk>
2020-07-09 17:15:27 +0000

    Changes made from 279 to 281

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,44 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
   
+
   def search
-    render json: params[:friend]
+    if params[:friend]
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result', layout: false }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result', layout: false }
+        end 
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please entersfsfz"
+        format.js {render partial: 'users/friend_result', layout: false }
+      end
+    end
+        
+    # if params[:friend].present?
+    #   @friends = User.search(params[:friend])
+    #   if @friends
+    #     respond_to do |format|
+    #       format.js {render partial: 'users/friend_result' }
+    #     end
+    #   else
+    #     respond_to do |format|
+    #       flash.now[:alert] = "Couldn't find user"
+    #       format.js { render partial: 'users/friend_result' }
+    #     end
+    #   end
+    # else
+    #   respond_to do |format|
+    #     flash.now[:alert] = "Please enter a friend name or email to seach"
+    #     format.js { render partial: 'users/friend_result' }
+    #   end
+    # end
   end
 end
