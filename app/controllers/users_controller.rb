8f7233e902087e3231eac2e14b6ef0a90ac124d9
Malcolm Duck <m.o.duck.15@aberdeen.ac.uk>
2020-07-09 17:34:26 +0000

    Updated to 282 pre styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,42 +9,23 @@ class UsersController < ApplicationController
   
 
   def search
-    if params[:friend]
-      @friend = params[:friend]
-      if @friend
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      if @friends
         respond_to do |format|
-          format.js { render partial: 'users/friend_result', layout: false }
+          format.js {render partial: 'users/friend_result' }
         end
       else
         respond_to do |format|
           flash.now[:alert] = "Couldn't find user"
-          format.js { render partial: 'users/friend_result', layout: false }
-        end 
+          format.js { render partial: 'users/friend_result' }
+        end
       end
     else
       respond_to do |format|
-        flash.now[:alert] = "Please entersfsfz"
-        format.js {render partial: 'users/friend_result', layout: false }
+        flash.now[:alert] = "Please enter a friend name or email to seach"
+        format.js { render partial: 'users/friend_result' }
       end
     end
-        
-    # if params[:friend].present?
-    #   @friends = User.search(params[:friend])
-    #   if @friends
-    #     respond_to do |format|
-    #       format.js {render partial: 'users/friend_result' }
-    #     end
-    #   else
-    #     respond_to do |format|
-    #       flash.now[:alert] = "Couldn't find user"
-    #       format.js { render partial: 'users/friend_result' }
-    #     end
-    #   end
-    # else
-    #   respond_to do |format|
-    #     flash.now[:alert] = "Please enter a friend name or email to seach"
-    #     format.js { render partial: 'users/friend_result' }
-    #   end
-    # end
   end
 end
